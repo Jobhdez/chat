a924f371bcfcc8e4082558723f4d431d4fcd22e6
Job <lambdainsphere@Jobs-MacBook-Air.local>
2022-06-09 11:41:31 -0700

    finish urls

--- backend/philosophy_chat/chat/urls.py
+++ backend/philosophy_chat/chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'chat'
 
 urlpatterns = [
-    path('subject/', views.subject_chat_room,
-         name='subject_chat_room'),
-    ]
+    path('', views.index, name='index'),
+    path('<str:room_name>/', views.room, name='room'),
+]
