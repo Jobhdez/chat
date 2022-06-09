8a29064ea0d6e7fa79daa82d17509452014cea3b
Job <lambdainsphere@Jobs-MacBook-Air.local>
2022-06-09 11:44:26 -0700

    begin writing Home.

--- frontend/chat/src/components/Home.js
+++ frontend/chat/src/components/Home.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { w3cwebsocket as WebSocket } from 'websocket';
+
+
 
 function Home() {
+
+    const [login, setLogin] = useState('false');
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState('');
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('philosophy');
+
+    const client = new WebSocket('ws://')
+
+
     return(
-         <h1>
-             hello!
-         </h1>
+         
     );
 }
 
