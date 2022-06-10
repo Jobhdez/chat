2434e4a923c842bcd74a57fe9de72effb55ef17f
Job <hj93@protonmail.com>
2022-06-10 12:26:45 -0700

    add host.

--- backend/philosophy_chat/philosophy_chat/settings.py
+++ backend/philosophy_chat/philosophy_chat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -131,10 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ASGI_APPLICATION = 'philosophy_chat.routing.application'
 
 CHANNEL_LAYERS = {
-  'default': {
-      'BACKEND': 'channels_redis.core.RedisChannelLayer',
-      'CONFIG': {
-          'hosts': [('127.0.0.1', 6379)],
-      },
-  },
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        },
+    },
 }
